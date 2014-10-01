d183b90b724371fea054291ba9a90d15ca02a96a
Anand Kumar <anand@anandkumar.net>
2014-10-02 00:23:56 +0530

    Revert "Revert "Removed Custom Header images""
    This reverts commit d559596592c7244669e636dbbaffbc937e6dbbfa.

--- header.php
+++ header.php
@@ -13,34 +13,22 @@
 			<a href="#content" class="screen-reader-text"><?php _e( 'Skip to content', 'stargazer' ); ?></a>
 		</div><!-- .skip-link -->
 
-		<?php hybrid_get_menu( 'primary' ); // Loads the menu/primary.php template. ?>
+		<header <?php hybrid_attr( 'header' ); ?>>
+
+				<div id="branding">
+					<?php hybrid_site_title(); ?>
+					<?php hybrid_site_description(); ?>
+				</div><!-- #branding -->
+
+		</header><!-- #header -->
+
+		<?php hybrid_get_menu( 'primary' ); // Loads the menu/primary.php template. 
+		
+		
+		if ( function_exists( 'soliloquy' ) ) { soliloquy( '2538', 'slug' ); }
+		?>
 
 		<div class="wrap">
-
-			<header <?php hybrid_attr( 'header' ); ?>>
-
-				<?php if ( display_header_text() ) : // If user chooses to display header text. ?>
-
-					<div id="branding">
-						<?php hybrid_site_title(); ?>
-						<?php hybrid_site_description(); ?>
-					</div><!-- #branding -->
-
-				<?php endif; // End check for header text. ?>
-
-				<?php hybrid_get_menu( 'secondary' ); // Loads the menu/secondary.php template. ?>
-
-			</header><!-- #header -->
-
-			<?php if ( get_header_image() && !display_header_text() ) : // If there's a header image but no header text. ?>
-
-				<a href="<?php echo home_url(); ?>" title="<?php echo esc_attr( get_bloginfo( 'name' ) ); ?>" rel="home"><img class="header-image" src="<?php header_image(); ?>" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt="" /></a>
-
-			<?php elseif ( get_header_image() ) : // If there's a header image. ?>
-
-				<img class="header-image" src="<?php header_image(); ?>" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt="" />
-
-			<?php endif; // End check for header image. ?>
 
 			<div id="main" class="main">
 
